c47769671c22e24c6f2e98bd85f93d2f5e4dc432
hongleir2 <hongleir2@gmail.com>
2025-02-23 18:49:24 -0800

    change something in Login.tsx

--- taskmaster-pro/frontend/src/components/Login.tsx
+++ taskmaster-pro/frontend/src/components/Login.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import useStore from '../store';
 
 const Login: React.FC = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { setUser } = useStore();
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const { setUser } = useStore();
+    return (<>Login</>)
 };
 
 export default Login;
